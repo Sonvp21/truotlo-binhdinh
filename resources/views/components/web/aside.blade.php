9edfe17668cda1e6bb623b9a3a1a37d6e49a774c
Sonvp21 <nguyensonvp21@gmail.com>
2024-07-19 11:01:57 +0700

    merger next

--- resources/views/components/web/aside.blade.php
+++ resources/views/components/web/aside.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div>
     <aside>
         <div class="w-80 space-y-4">
@@ -7,11 +6,3 @@
         </div>
     </aside>
 </div>
-=======
-<aside>
-    <div class="w-80 space-y-4">
-        <x-web.aside.weather-forecast />
-        <x-web.aside.quick-forecast />
-    </div>
-</aside>
->>>>>>> 683bbfeddd004eb38bb596b7f24d4996019df57a
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,7 +25,6 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-<<<<<<< HEAD
     <x-rich-text::styles
         theme="richtextlaravel"
         data-turbo-track="false"
@@ -44,18 +43,6 @@
         </div>
 
         <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
-=======
-</head>
-<body class="font-sans text-gray-900 antialiased">
-    <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
-        <div>
-            <a href="/">
-                <x-application-logo class="h-20 w-20 fill-current text-gray-500" />
-            </a>
-        </div>
-
-        <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
->>>>>>> 683bbfeddd004eb38bb596b7f24d4996019df57a
             {{ $slot }}
         </div>
     </div>
